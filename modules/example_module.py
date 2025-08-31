5bbf42a352626ced9bcc4843a9963ba9265d2ec5
JCMRS <jcmrs@users.noreply.github.com>
2025-08-31 16:57:29 +0200

    Add example module as plug-and-play template

---
+++ modules/example_module.py
@@ -0,0 +1,11 @@
+"""
+Example Module for Copilot Memory
+
+Modules are designed to be plug-and-play for new features.
+"""
+
+def run():
+    print("Example module running.")
+
+if __name__ == "__main__":
+    run()
